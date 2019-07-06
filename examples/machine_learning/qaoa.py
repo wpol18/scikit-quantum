fd781c5321a3203a6ab216b92d268606fd33c8d0
Mathew Kallada <matkallada@gmail.com>
2019-07-06 11:25:12 -0400

    Work on QAOA

--- examples/machine_learning/qaoa.py
+++ examples/machine_learning/qaoa.py
@@ -7,12 +7,48 @@ Builds a basic QAOA with PennyLane
 import pennylane as qml
 from pennylane import numpy as np
 from scipy.stats import unitary_group
+import networkx as nx
 
 n_features = 2
 num_layers = 1
 var = 0.05 * np.random.randn(num_layers, n_features*7)
 
 dev = qml.device('default.qubit', wires=n_features)
+
+
+import numpy as np
+
+def graph_cuts(graph: nx.Graph) -> np.ndarray:
+    """For the given graph, return the cut value for all binary assignments
+    of the graph.
+    """
+
+    N = len(graph)
+    diag_hamiltonian = np.zeros(shape=([2]*N), dtype=np.double)
+    for q0, q1 in graph.edges():
+        for index, _ in np.ndenumerate(diag_hamiltonian):
+            if index[q0] != index[q1]:
+                weight = graph[q0][q1].get('weight', 1)
+                diag_hamiltonian[index] += weight
+
+    return diag_hamiltonian
+
+
+def hamiltonian(omega=1.0, ampl0=0.2):
+    """Two-level-system Hamiltonian
+
+    Args:
+        omega (float): energy separation of the qubit levels
+        ampl0 (float): constant amplitude of the driving field
+    """
+    # .full() converts everything to numpy arrays
+    H0 = -0.5 * omega * np.array([[-1, 0], [0, 1]], dtype=np.complex128)
+    H1 = np.array([[0, 1], [1, 0]], dtype=np.complex128)
+
+    return H0, H1
+
+print(hamiltonian())
+
 
 
 @qml.qnode(dev)
@@ -38,6 +74,10 @@ def _qaoa_circuit(var, betas, gammas):
     # The hamiltonian a hermitian operator. It is not a quantum gate.
     # A hermitian must be exponentiated to be a unitary. Unitaries are quantum gates.
 
+    H0, H1 = hamiltonian()
+    graph = nx.gnp_random_graph(4, 0.5)
+    H1 = graph_cuts(graph)
+
     J = np.array([[0,1],[0,0]])
     steps = 10
     betas = np.random.uniform(0, np.pi*2, steps)
@@ -47,22 +87,16 @@ def _qaoa_circuit(var, betas, gammas):
         # Run Driver Hamiltonian
         for i in range(n_features):
             for j in range(n_features):
-                # phi = -J[i, j]
-                # rz_1 = [[np.e**(-j*phi/2), 0],[0, np.e**(j*phi/2)]]
-                # phi = 1.0
-                # rz_2 = [[np.e**(-j*phi/2), 0],[0, np.e**(j*phi/2)]]
 
-                # if i!=j:
-                #     qml.QubitUnitary(np.kron(rz_1, rz_2), wires=[i, j])
+                # We to hit the quantum state with the product of the exponential of the Hamiltonian Cost
+                if i!=j:
+                    qml.QubitUnitary(np.e**H0, wires=[i, j])
 
-                # qml.RZ(-J[i, j], wires=i)
-                # qml.RZ(1.0, wires=j)
 
         # Run Mixer Hamiltonian
         for i in range(n_features):
-            qml.RX(-betas[n_step], wires=i)
+            qml.QubitUnitary(np.e**H1, wires=[i])
 
-    # We to hit the quantum state with the product of the exponential of the Hamiltonian Driver
     return qml.expval.PauliX(0)
 
 print(_qaoa_circuit(var, [], []))
